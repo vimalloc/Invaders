204bb4f25de8a01bf43e9d44b48e1187ed4b8521
Landon GB <landogbland@gmail.com>
2011-04-04 22:53:53 -0600

    Adds a ship object

---
+++ src/ship.h
@@ -0,0 +1,16 @@
+/*
+ * Ship.h
+ *
+ * Author: Landon Gilbert-Bland
+ */
+
+#ifndef SHIP_H
+#define SHIP_H
+
+/* Handle for a ship */
+typedef struct {
+    int xpos;
+    int ypos;
+} ship;
+
+#endif
