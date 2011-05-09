a9507f5bdd597f64af406e447e42d3b98668ba58
Landon GB <landogbland@gmail.com>
2011-05-09 11:18:07 -0600

    Add more thoughts to the bullet implementation

--- src/bullet.h
+++ src/bullet.h
@@ -16,8 +16,27 @@ typedef struct {
 /* Call the function pointer with the old x and y position, and get the
  * newly updated x and y positions */
 
+
 /* FUNCTION POINTER SIGNITURE IDEA
-void bullet_update_position(bullet *b, int *xpos, int *ypos, int bullet_number);
+
+X and Y position, function pointer, and bullet number stored inside the bullet
+void bullet_update_position(bullet *b);
+*/
+
+
+/* Function to create different types of bullets, these will call hte bull_create functions bellow
+
+void bullet_create_basic(int level);
+void bullet_create_spread(int level);
+void bullet_create_fast(int level);
+void bullet_create_etc(int level);
+*/
+
+
+/* Need a way to create the initial bullet, based on the starting x an y position. This
+   should be called by the bullet_create_ functions above
+
+void bullet_creates(bullet **bullet[], number_of_bullets, start_x, start_y, sprite, update_function);
  */
 
 #endif
